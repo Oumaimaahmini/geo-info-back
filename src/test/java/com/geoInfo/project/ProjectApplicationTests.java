aa21fef62285af77fe8c76a142a1775f2c076db6
Basma Senani <basmasn71@gmail.com>
2021-10-29 09:26:57 +0100

    initial commit

---
+++ src/test/java/com/geoInfo/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geoInfo.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
